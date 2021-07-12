81a641a064929a5b0f2ed1b141ff1ce05ae356e7
shivamsrivastava0591 <shubham.sri4641@gmail.com>
2021-07-12 15:11:44 +0530

    First Commit

---
+++ react-ts/src/App.tsx
@@ -0,0 +1,19 @@
+import NewTodo from './components/NewTodo';
+import Todos from './components/Todos';
+import Todo from './models/todo';
+import TodosContextProvider from './store/todos-context';
+// import {useState} from 'react';
+
+function App() {
+ 
+
+  return (
+    <TodosContextProvider>
+      <NewTodo />
+      <Todos />
+    </TodosContextProvider>
+    
+  );
+}
+
+export default App;
